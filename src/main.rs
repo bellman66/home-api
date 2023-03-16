2ba854efed6cb71d6fac6aa6a989e57534023d6e
rndso15 <rndso15@naver.com>
2023-03-16 18:44:54 +0900

    feat: file system

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::net::SocketAddr;
 use axum::{routing::get, Router, Server};
 
+mod domain;
+use domain::account;
+
 #[tokio::main]
 async fn main() {
     // Set Default Router, Addr
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let router = get_router().await;
+    let router = get_router();
 
     Server::bind(&addr)
         .serve(router.into_make_service())
@@ -13,9 +16,10 @@ async fn main() {
         .unwrap();
 }
 
-async fn get_router() -> Router {
+fn get_router() -> Router {
     Router::new()
         .route("/", get(root))
+        .route("/status", get(account::controller::accountcontroller::get_status))
 }
 
 // basic handler that responds with a static string
