fd542910a31ed37cfe5e236c15cc8d26b9164482
rndso15 <rndso15@naver.com>
2023-05-01 21:05:04 +0900

    feat : postgre sql setting

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod context;
 mod domain;
 use domain::account;
 use std::collections::HashMap;
+use std::process::id;
+use tokio_postgres::{Config, Error, NoTls};
 use crate::domain::index;
 
 #[macro_use]
@@ -23,9 +25,26 @@ lazy_static! {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     // Set Default Router, Addr
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // Connect Postgre
+    let (client, connection) = Config::new().host("localhost").user("rndso15").port(5432).password("ring123").dbname("homeapi").connect(NoTls).await.unwrap();
+
+    // Connection Spawn
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error : {}", e);
+        }
+    });
+
+    let mut rows = client.query("SELECT * FROM account", &[]).await?;
+    for row in rows {
+        let id:i32 = row.get("id");
+        let email:&str = row.get("email");
+        println!("{} / {}", id, email);
+    }
 
     // First access to `HASHMAP` initializes it
     println!("The entry for `0` is \"{}\".", HASHMAP.get(&0).unwrap());
@@ -34,6 +53,8 @@ async fn main() {
         .add_route("/", get(root))
         .add_route("/status", get(index::controller::index_controller::get_status))
         .run().await;
+
+    Ok(())
 }
 
 // basic handler that responds with a static string
